ac5272ae5ca5cf33f44583fe7dbed92db335802e
Benfarhat <benfarhat.elyes@gmail.com>
2020-02-05 21:56:47 -0500

    Initial commit

---
+++ SimpleWebsocket/src/test/java/ca/benfarhat/simple/websocket/SimpleWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.benfarhat.simple.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
