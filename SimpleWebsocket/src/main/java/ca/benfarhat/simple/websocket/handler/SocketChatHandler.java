ca07486a23108ff0337278aade1c9b8cb767a5ea
Benfarhat <benfarhat.elyes@gmail.com>
2020-02-10 22:17:38 -0500

    Gestion utilisateur/ broadcast / surveillance connexion

--- SimpleWebsocket/src/main/java/ca/benfarhat/simple/websocket/handler/SocketChatHandler.java
+++ SimpleWebsocket/src/main/java/ca/benfarhat/simple/websocket/handler/SocketChatHandler.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.util.Strings;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,21 +21,22 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class SocketChatHandler extends TextWebSocketHandler {
 	private static final Logger log = LoggerFactory.getLogger(SocketChatHandler.class);
-	Map<WebSocketSession, String> sessions = new HashMap<>();
-	List<String> messagesHistory = new ArrayList<>();
-	
+	private Map<WebSocketSession, String> sessions = new HashMap<>();
+	private List<String> messagesHistory = new ArrayList<>();
+	private List<String> usernames = new ArrayList<String>();
+
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
-		
+
 		String payload = message.getPayload();
-		
+
 		// On en profite pour recupérer le nom de l'utilisateur
 		JSONObject jsonObject = new JSONObject(payload);
 		sessions.replace(session, null, (String) jsonObject.get("user"));
 
-		switch(String.valueOf(jsonObject.get("action"))) {
+		switch (String.valueOf(jsonObject.get("action"))) {
 		case "sendMessage":
-			sessions.forEach((webSocketSession,username)->{
+			sessions.forEach((webSocketSession, username) -> {
 				try {
 					System.out.println(payload);
 					webSocketSession.sendMessage(new TextMessage(payload));
@@ -41,50 +45,87 @@ public class SocketChatHandler extends TextWebSocketHandler {
 					log.error(e.getMessage());
 				}
 			});
-		break;
+			break;
 		case "changeName":
+			String oldName = sessions.get(session);
+			String newName = String.valueOf(jsonObject.get("user"));
 			sessions.replace(session, String.valueOf(jsonObject.get("user")));
-			List<String> usernames = new ArrayList<String>();
-			sessions.forEach((webSocketSession,username)->usernames.add(username));
-
-			sessions.forEach((webSocketSession,username)->{
-				try {
-					JSONObject response = new JSONObject();
-					response.append("action", "updateUsername");
-					response.append("usernames", usernames);
-					webSocketSession.sendMessage(new TextMessage(response.toString()));
-
-				} catch (IOException e) {
-					log.error(e.getMessage());
-				}	
-			});
+			sendUserList();
+			if(Objects.isNull(oldName)) {
+				if(Strings.isBlank(newName)) {
+					broadcastInformation(sessions, "Un utilisateur a rejoint la session");
+				} else {
+					broadcastInformation(sessions, newName + " a rejoint la session");
+				}
+				
+			} else {
+				broadcastInformation(sessions, oldName + " a changé son nickname en " + newName);
+			}
 
 			break;
 		default:
 		}
 
-
 	}
 
-	@Override
+	private void sendUserList() {
+
+		usernames = sessions.entrySet().stream().filter(session -> !Strings.isEmpty(session.getValue()))
+				.map(Map.Entry::getValue).collect(Collectors.toList());
+
+		sessions.forEach((webSocketSession, username) -> {
+
+			JSONObject response = new JSONObject();
+			response.append("action", "updateUsername");
+			response.append("usernames", usernames);
+			try {
+				webSocketSession.sendMessage(new TextMessage(response.toString()));
+			} catch (IOException e) {
+				log.info(e.getMessage());
+			}
+
+		});
+	}
+
+	@Override 
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		
+		String username = sessions.get(session);
+		
 		sessions.remove(session);
 		log.info("Un utilisateur a quitter la conversation.");
-	}
-
+		sendUserList();
+		broadcastInformation(sessions, username + " a quitté la session");
+	} 
+ 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sessions.put(session, null);
 		log.info("Un utilisateur a rejoint la conversation.");
-		
-		messagesHistory.forEach(payload ->{
+		sendUserList();
+		messagesHistory.forEach(payload -> {
 			try {
 				session.sendMessage(new TextMessage(payload));
 			} catch (IOException e) {
 				log.error(e.getMessage());
 			}
 		});
-		
+
+	}
+
+	public void broadcastInformation(Map<WebSocketSession, String> sessions, String message) {
+		sessions.forEach((webSocketSession, username) -> {sendInformation(webSocketSession, message);});
+	}
+
+	public void sendInformation(WebSocketSession session, String message) {
+		JSONObject response = new JSONObject();
+		response.append("action", "information");
+		response.append("message", message);
+		try {
+			session.sendMessage(new TextMessage(response.toString()));
+		} catch (IOException e) {
+			log.info(e.getMessage());
+		}
 	}
 
 }
